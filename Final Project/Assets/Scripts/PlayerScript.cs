2df0194c357c61316a2f94c79b9aec57009efa8f
Simon Steele <simon.steele@mymail.champlain.edu>
2019-12-04 23:14:31 -0500

    Added third person controller to game as well

--- Final Project/Assets/Scripts/PlayerScript.cs
+++ Final Project/Assets/Scripts/PlayerScript.cs
@@ -15,21 +15,30 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float inputAmountX = Mathf.Abs(Input.GetAxis("Horizontal"));
+        float inputAmountY = Mathf.Abs(Input.GetAxis("Vertical"));
+
+        GetComponentInChildren<Animator>().SetFloat("Forward", Mathf.Clamp(inputAmountX + inputAmountY, 0, 1), 0.1f, Time.deltaTime);
+
         if (Input.GetKey(KeyCode.W))
         {
             GetComponent<Particle3D>().AddForceAtPoint(new Vector3(0.0f, 0.0f, 0.1f), GetComponent<Particle3D>().Mass * Vector3.forward);
+            transform.GetChild(0).localEulerAngles = new Vector3(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Particle3D>().AddForceAtPoint(new Vector3(-0.1f, 0.0f, 0.0f), GetComponent<Particle3D>().Mass * Vector3.left);
+            transform.GetChild(0).localEulerAngles = new Vector3(0, 270, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
             GetComponent<Particle3D>().AddForceAtPoint(new Vector3(0.0f, 0.0f, -0.1f), GetComponent<Particle3D>().Mass * Vector3.back);
+            transform.GetChild(0).localEulerAngles = new Vector3(0, 180, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
             GetComponent<Particle3D>().AddForceAtPoint(new Vector3(0.1f, 0.0f, 0.0f), GetComponent<Particle3D>().Mass * Vector3.right);
+            transform.GetChild(0).localEulerAngles = new Vector3(0, 90, 0);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
