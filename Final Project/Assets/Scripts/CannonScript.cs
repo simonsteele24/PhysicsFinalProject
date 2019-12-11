7cfd22150a9c0e762b338caf9b1ed23ee57804e2
Simon Steele <simon.steele@mymail.champlain.edu>
2019-12-11 16:33:33 -0500

    Added cannon which shoots bombs

---
+++ Final Project/Assets/Scripts/CannonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    public float rotation;
+    public GameObject bomb;
+    public float bombForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationVertical = Input.GetAxis("Vertical");
+        float rotationHorizontal = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.forward, rotationVertical * rotation * Time.deltaTime);
+        transform.Rotate(Vector3.up, rotationHorizontal * rotation * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var newBomb = Instantiate(bomb, transform.position,Quaternion.identity);
+            newBomb.GetComponent<Particle3D>().AddForce(transform.right * bombForce);
+        }
+    }
+}
