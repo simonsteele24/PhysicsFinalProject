0c4a41d833e9ceea5bcec329fbe68981caa3e927
Simon Steele <simon.steele@mymail.champlain.edu>
2020-01-03 16:03:10 -0500

    Revamped ai for Bob omb

---
+++ Final Project/Assets/Scripts/AI/BobombIdleState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobombIdleState : State
+{
+    public float rotationSpeed;
+    public float distanceToChasePlayer = 5;
+    GameObject player;
+
+    public override States CheckForTransition()
+    {
+        float distance = Mathf.Abs(Vector3.Distance(transform.parent.position, player.transform.position));
+
+        if (distance < distanceToChasePlayer)
+        {
+            Vector3 direction = player.transform.position - transform.parent.position;
+            float orientation = Vector3.Dot(direction, transform.parent.forward);
+
+            if (orientation > 0)
+            {
+                return States.Chase;
+            }
+        }
+
+        return States.Idle;
+    }
+
+    public override void OnEnterState()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    public override void OnExitState() { }
+
+    public override void UpdateState()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        GetComponentInParent<Goomba>().MoveInADirection(transform.forward);
+    }
+}
